44e87828157cc35c310e3a2d3c907310eb20d0ae
onoeserika007 <984426676@qq.com>
2026-05-30 04:46:22 +0800

    google test lib added

---
+++ build-ninja/ThirdParty/assimp/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/UGit/Serika-Renderer-OpenGL/ThirdParty/assimp/contrib/zlib
+# Build directory: D:/UGit/Serika-Renderer-OpenGL/build-ninja/ThirdParty/assimp/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build-ninja/ThirdParty/googletest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: D:/UGit/Serika-Renderer-OpenGL/ThirdParty/googletest
+# Build directory: D:/UGit/Serika-Renderer-OpenGL/build-ninja/ThirdParty/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googletest")
