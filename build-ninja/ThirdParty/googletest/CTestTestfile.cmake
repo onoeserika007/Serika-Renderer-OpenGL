5877072035579c9440c3205ac990e272571cadde
onoeserika007 <984426676@qq.com>
2026-05-30 05:56:51 +0800

    remove build

--- build-ninja/ThirdParty/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/UGit/Serika-Renderer-OpenGL/ThirdParty/googletest
-# Build directory: D:/UGit/Serika-Renderer-OpenGL/build-ninja/ThirdParty/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
--- build-ninja/ThirdParty/pfr/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/UGit/Serika-Renderer-OpenGL/ThirdParty/pfr
-# Build directory: D:/UGit/Serika-Renderer-OpenGL/build-ninja/ThirdParty/pfr
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
